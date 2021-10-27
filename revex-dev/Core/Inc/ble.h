5b86faafe4a47ca97c152b161feb84c874d61392
ihagedo <isaachagedorn@gmail.com>
2021-10-27 01:30:33 -0400

    restructured dev

--- revex-dev/Core/Inc/ble.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BLE_H__
-#define	__BLE_H__
-
-void BLE_Init();
-
-void BLE_transmit(uint8_t * data);
-
-#endif	/* __BLE_H__ */
--- revex-dev/Core/Inc/pwm.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PWM_H__
-#define	__PWM_H__
-
-void set_freq(int freq);
-
-void set_duty(int duty);
-
-#endif	/* __PWM_H__ */
