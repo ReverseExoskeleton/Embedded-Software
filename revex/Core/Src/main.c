335cf7f7f1e33303541f18743ed9babd5a20a397
Rumple <rumple0@purdue.edu>
2021-12-01 20:41:47 -0500

    Fixed IMU scale init issue WIP
    IMU not changing sometimes, also will be looking into saving offset bias for a one-time calibration

--- revex/Core/Src/main.c
+++ revex/Core/Src/main.c
@@ -18,7 +18,6 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "imu.h"
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
@@ -431,7 +430,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM3_Init();
   MX_I2C1_Init();
-  //MX_SPI1_Init();
+  MX_SPI1_Init();
   MX_USART1_UART_Init();
   MX_DMA_Init();
   MX_TIM6_Init();
